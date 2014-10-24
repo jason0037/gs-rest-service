6a395e9fa6bb9d88db01b1ab32367343555db452
jaychenq <jason0037@163.com>
2014-10-24 14:32:09 +0800

    change service port

--- initial/src/main/java/hello/Application.java
+++ initial/src/main/java/hello/Application.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 
 @ComponentScan
 @EnableAutoConfiguration
+@PropertySource("classpath:application.properties")
 public class Application {
 
     public static void main(String[] args) {
